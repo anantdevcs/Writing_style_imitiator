7c89f2e8436f0a642c90c283c87e81e99bd945bf
anantdevcs <anantdeviitism@gmail.com>
2019-12-28 01:15:32 +0530

    Added the results file

--- app.py
+++ app.py
@@ -2,15 +2,19 @@ from flask import Flask, render_template
 from flask import request, redirect
 
 app = Flask(__name__)
-
+it_text = ""
 @app.route('/',methods=['POST', 'GET'])
 
 def index():
     if request.method == "POST":
         req = request.form
-        print(req)
-    return render_template('index.html')
+        it_text = req['message']
+        return redirect('/result/'+it_text)
 
+    return render_template('index.html')
+@app.route('/result/<result>')
+def result(result):
+    return render_template('result.html', result = result)
 
 if __name__ == "__main__":
     app.run(debug=True)
